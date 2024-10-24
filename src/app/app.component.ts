ccd3c748b3a8ec689c672c9bdaa4f386fabd7bd5
Dmitriy <john.smith.dm8338@gmail.com>
2024-10-25 00:42:47 +0300

    - loader screen for update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SwUpdate } from '@angular/service-worker';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { interval } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -21,12 +23,15 @@ export class AppComponent {
   swUpdate = inject(SwUpdate);
   constructor() {
     if (this.swUpdate.isEnabled) {
-      this.swUpdate.versionUpdates.subscribe((event) => {
-        if (event.type === 'VERSION_READY') {
-          this.activateUpdate();
-          // this.updateAvailable = true;
-        }
-      })
+      this.swUpdate.versionUpdates
+        .pipe(takeUntilDestroyed())
+        .subscribe((event) => {
+          if (event.type === 'VERSION_READY') {
+            this.startUpdating(); // start update with progress bar
+            // this.activateUpdate(); // auto update
+            // this.updateAvailable.set(true); // manual update
+          }
+        })
     }
   }
   /** auto update */
@@ -35,8 +40,37 @@ export class AppComponent {
   }
 
   /** manual update */
-  updateAvailable = false;
+  updateAvailable = signal(false);
   reloadApp(): void {
     this.swUpdate.activateUpdate().then(() => document.location.reload());
+  }
+
+  /** progress bar */
+  progress = signal(0);
+  estimatedTime = signal(30);
+  startUpdating() {
+    this.updateAvailable.set(true);
+
+    let progress = 0;
+    let estimatedTime = 30;
+
+    const timer = interval(1000).subscribe(() => {
+      progress += 100 / estimatedTime;
+      estimatedTime--;
+
+      this.progress.set(progress);
+      this.estimatedTime.set(estimatedTime);
+
+      if (progress >= 100) {
+        timer.unsubscribe();
+        this.completeUpdate();
+      }
+    });
+  }
+
+  completeUpdate() {
+    this.swUpdate.activateUpdate().then(() => {
+      document.location.reload();
+    });
   }
 }
